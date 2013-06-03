818d92e86c3b34ddae2b8007b463ec1a5d79c1ff
Joaquin <joaduo@gmail.com>
2013-06-02 22:24:10 -0300

    Before general refactoring.

--- mepinta/developer_tools/refactoring/Renaming.sh
+++ mepinta/developer_tools/refactoring/Renaming.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+REFACTORING_DIR=""
+
 function replace_in_text(){
   for f in $FILES ; do 
-    #echo "On file $f"
-    echo "$RENAMING_DICT" | while read LINE ; do
-      FROMTX=$( echo "$LINE" | cut -d" " -f1)
-      TOTX=$( echo "$LINE" | cut -d" " -f2)
-      
-      if [ "$1" = "-y" ] ; then
-        cmd="sed 's/$FROMTX/$TOTX/g' -i $f"
-      else
-        #cmd="sed 's/$FROMTX/$TOTX/g' $f | sed '/$TOTX/ p' -n"
-        cmd="sed 's/$FROMTX/$TOTX/g' $f | grep '$TOTX' --color"
-      fi
-      eval $cmd
-    done 
+    if [ $(echo "$f" | grep -v "developer_tools/refactoring") ] ; then
+      echo "$RENAMING_DICT" | while read LINE ; do
+        FROMTX=$( echo "$LINE" | cut -d" " -f1)
+        TOTX=$( echo "$LINE" | cut -d" " -f2)
+        
+        if [ "$1" = "-y" ] ; then
+          echo "Replacing on $f"
+          cmd="sed 's/$FROMTX/$TOTX/g' -i $f"
+        else
+          #cmd="sed 's/$FROMTX/$TOTX/g' $f | sed '/$TOTX/ p' -n"
+          cmd="sed 's/$FROMTX/$TOTX/g' $f | grep '$TOTX' --color"
+        fi
+        eval $cmd
+      done 
+
+    fi
   done
 }
 
@@ -35,18 +40,20 @@ function rename_files(){
   done 
 }
 
-RENAMING_DICT="EclipseCoreProjectTemplates EclipsePydevProjectTemplates
-EclipseCoreProjectCreator EclipsePydevProjectCreator"
+RENAMING_DICT="$(python ./convert_functions_names.py)"
 
 # FILES=$(find ./ -iname "*.cpp"); replace_in_text $1
 # FILES=$(find ./ -iname "*.hpp"); replace_in_text $1
 # FILES=$(find ./ -iname "*.c"); replace_in_text $1
 # FILES=$(find ./ -iname "*.h") ; replace_in_text $1
 
+cd ../../
+
+#echo "$RENAMING_DICT"
 FILES=$(find ./ -iname "*.py") ; replace_in_text $1
 
-FILES_RENAMING_DICT=$RENAMING_DICT
-rename_files $1
+# FILES_RENAMING_DICT=$RENAMING_DICT
+# rename_files $1
 
 if [ "$1" = "-y" ] ; then
   echo "Running the commands!"
