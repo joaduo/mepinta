87b814f85f4cea7c5b29a8407b52cc06a4ad8013
Joaquin <joaduo@gmail.com>
2013-12-02 04:15:54 -0300

    Add command flags for forcing generation and build

--- mepinta/developer_tools/python_tools/mepinta_devtools/python_project/shedskin_project/templates_repository/build_common.py
+++ mepinta/developer_tools/python_tools/mepinta_devtools/python_project/shedskin_project/templates_repository/build_common.py
@@ -23,6 +23,7 @@ import subprocess
 import os
 import shutil
 import shlex
+import argparse
 
 try:
   import shedskin
@@ -30,72 +31,113 @@ except Exception as e:
   msg = 'You need Shedskin installed in your Python path. Check http://code.google.com/p/shedskin/.\n'
   raise ImportError('%s  ImportError:%s' % (msg, e))
 
-def logInfo(msg):
-  print(msg)
+class ShedskinModuleBuilder(object):
+  def logInfo(self, msg):
+    print(msg)
 
-def logError(msg):
-  sys.stderr.write(msg + '\n')
+  def logError(self, msg):
+    sys.stderr.write(msg + '\n')
 
-def copyShedskinModule(python_module):
-  module_lib = '%s.so' % python_module
-  dst_module_lib = 'mepinta/pipeline/lo_cpp/%s' % module_lib
-  if os.path.exists(module_lib):
-    if os.path.exists(dst_module_lib):
-      os.remove(dst_module_lib)
-    shutil.copy(module_lib, dst_module_lib)
+  def copyShedskinModule(self, python_module):
+    #Copy the compiled .so module to the destination directory
+    module_lib = '%s.so' % python_module
+    dst_module_lib = 'mepinta/pipeline/lo_cpp/%s' % module_lib
+    if os.path.exists(module_lib):
+      if os.path.exists(dst_module_lib):
+        os.remove(dst_module_lib)
+      shutil.copy(module_lib, dst_module_lib)
 
-def getMakefileName(python_module):
-  return '%s_Makefile' % python_module
+  def getMakefileName(self, python_module):
+    #Each module will get its own Makefile (in order to coexist together)
+    return '%s_Makefile' % python_module
 
-def generateShedskinCppCode(python_module, cmd_args=[]):
-  # First generate the CPP code
-  sys.argv.append('-e') #create extension
-  sys.argv.append('-n') #silent
-  makefile = getMakefileName(python_module)
-  sys.argv.extend(['-m', makefile]) #output makefile name
-  sys.argv.extend(['-L', '../shedskin_builtin_lib']) #other builtin libs
-  flags_file = '%s_FLAGS' % python_module #compiler flags file
-  if os.path.exists(flags_file): #if the file exist use it
-    sys.argv.extend(['-f', flags_file])
-  sys.argv.extend(cmd_args) #add other args
-  sys.argv.append(python_module) #python module to convert
+  def generateShedskinCppCode(self, python_module, cmd_args=[]):
+    # First generate the CPP code simulating the program arguments.
+    sys.argv.append('-e') #create extension
+    sys.argv.append('-n') #silent
+    makefile = self.getMakefileName(python_module)
+    sys.argv.extend(['-m', makefile]) #output makefile name
+    sys.argv.extend(['-L', '../shedskin_builtin_lib']) #other builtin libs
+    flags_file = '%s_FLAGS' % python_module #compiler flags file
+    if os.path.exists(flags_file): #if the file exist use it
+      sys.argv.extend(['-f', flags_file])
+    sys.argv.extend(cmd_args) #add other args
+    sys.argv.append(python_module) #python module to convert
 
-  logInfo('Generating shedskin module %s ...' % python_module)
-  #Run type analysis
-  shedskin.main()
-  logInfo('Done generating %s.' % python_module)
+    self.logInfo('Generating shedskin module %s ...' % python_module)
+    #Run type analysis
+    shedskin.main()
+    self.logInfo('Done generating %s.' % python_module)
 
-def compileShedskinModule(python_module):
-  # Now compile calling make
-  makefile = getMakefileName(python_module)
-  logInfo('Building shedskin module %s ...' % python_module)
-  cmd = 'make --makefile=%s' % makefile
-  cmd_split = shlex.split(cmd)
-  p = subprocess.Popen(cmd_split, stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-  r_value = p.wait()
-  stderr = p.stderr.read()
-  stdout = p.stdout.read()
-  if r_value != 0:
-    logError('There was an error building %s.so cmd=%r.' % (python_module, cmd))
-    logError(stderr)
-    logError(stdout)
-    sys.exit(r_value)
-  logInfo('Done building %s.' % python_module)
+  def compileShedskinModule(self, python_module):
+    # Now compile calling make
+    makefile = self.getMakefileName(python_module)
+    self.logInfo('Compiling and building shedskin module %s ...' % python_module)
+    cmd = 'make --makefile=%s' % makefile
+    cmd_split = shlex.split(cmd)
+    p = subprocess.Popen(cmd_split, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    r_value = p.wait()
+    stderr = p.stderr.read()
+    stdout = p.stdout.read()
+    if r_value != 0:
+      self.logError('There was an error building %s.so cmd=%r.' % (python_module, cmd))
+      self.logError(stderr)
+      self.logError(stdout)
+      self.sys.exit(r_value)
+    self.logInfo('Done building %s.' % python_module)
 
-def generateAndBuild(python_module, generate, build):
-  if generate:
-    #generate CPP code from python
-    generateShedskinCppCode(python_module)
-  if build:
-    #compile the CPP code
-    compileShedskinModule(python_module)
-    #copy the module to the mepinta code to be used
-    copyShedskinModule(python_module)
+  def generateAndBuild(self, python_module, generate, build):
+    if generate:
+      #generate CPP code from python
+      self.generateShedskinCppCode(python_module)
+    if build:
+      #compile the CPP code
+      self.compileShedskinModule(python_module)
+      #copy the module to the mepinta code to be used
+      self.copyShedskinModule(python_module)
 
-def buildShedskinModule(python_module, generate=True, build=True):
-  os.chdir(os.path.dirname(__file__))
-  generate = not os.path.exists(getMakefileName(python_module)) or \
-            len(sys.argv) > 1 and sys.argv[1] == '-f'
-  build = True
-  generateAndBuild(python_module, generate, build)
+  def _cleanModuleName(self, python_module):
+    python_module = os.path.basename(python_module)
+    python_module, _ = os.path.splitext(python_module)
+    return python_module
+
+  def run(self, argv=None, python_module=None):
+    #get the parser and parse the sys.argv arguments
+    os.chdir(os.path.dirname(__file__))
+    parser = self._getArgsParser(specify_module=not python_module)
+    args = parser.parse_args(argv)
+
+    #reset argv (used to pass arguments to shedksin)
+    sys.argv = sys.argv[:1]
+
+    python_module = self._cleanModuleName(python_module)
+    generate = not os.path.exists(self.getMakefileName(python_module)) or \
+              args.generate
+    build = not os.path.exists('%s.so' % python_module) or args.compile
+    if generate or build:
+      #We need to generate and/or compile.
+      self.generateAndBuild(python_module, generate, build)
+    else:
+      self.logInfo('Nothing to do.')
+
+  def _getArgsParser(self, specify_module=True):
+    parser = argparse.ArgumentParser(description='Mepinta Shedskin Generation '
+                                     'and Compilation Script.')
+    if specify_module:
+      help_ = 'Specify the python module name of the module to be translated.'
+      parser.add_argument('python_module', action='store', help=help_)
+    help_ = 'Force shedskin code generation.'
+    parser.add_argument('--generate', action='store_true', help=help_)
+    help_ = 'Force shedskin code compilation.'
+    parser.add_argument('--compile', action='store_true', help=help_)
+    help_ = 'Enable debug output.'
+    parser.add_argument('--debug', action='store_true', help=help_)
+    return parser
+
+def main(argv):
+  ShedskinModuleBuilder().run(argv)
+
+if __name__ == '__main__':
+  main(None)
+
