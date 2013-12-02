4593c1bc3aa9705d9b3f05c60f6d9d186f971212
Joaquin <joaduo@gmail.com>
2013-12-02 04:16:30 -0300

    Renamed

--- mepinta/developer_tools/python_tools/mepinta_devtools/python_project/shedskin_project/templates_repository/build_load_library_stand_alone_module.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from build_common import buildShedskinModule
-
-def build():
-  buildShedskinModule('load_library_stand_alone')
-
-if __name__ == '__main__':
-  build()
--- mepinta/developer_tools/python_tools/mepinta_devtools/python_project/shedskin_project/templates_repository/build_pipeline_lo_facade_module.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-from build_common import buildShedskinModule
-
-def build():
-  buildShedskinModule('pipeline_lo_facade')
-
-if __name__ == '__main__':
-  build()
