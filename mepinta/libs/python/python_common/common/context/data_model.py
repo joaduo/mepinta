52f4ad4befb4b6420ca270d7beea2a00815dab71
Joaquin <joaduo@gmail.com>
2014-01-30 08:18:53 -0300

    Add nice repr to data

--- mepinta/libs/python/python_common/common/context/data_model.py
+++ mepinta/libs/python/python_common/common/context/data_model.py
@@ -20,11 +20,12 @@ along with Mepinta. If not, see <http://www.gnu.org/licenses/>.
 '''
 import inspect
 from common.config.constants import global_config_namespace
+from pprint import pformat
 
 class BaseNode(object):
   def __init__(self):
     self._config = dict()
-    self._childs = set()
+    self._children = set()
 
   @property
   def config(self): #can only get the attribute
@@ -47,11 +48,40 @@ class BaseNode(object):
 
   def newChild(self):
     node = ChildNode(parent=self)
-    self._childs.add(node)
+    self._children.add(node)
     return node
 
   def getConfigDict(self):
     return dict(self._config)
+
+  def _reprDict(self, dictionary, indent=0):
+    result = '{'
+    for k in dictionary:
+      v = dictionary[k]
+      result += '\n%s%r:%r,' % (' ' * (indent + 2), k, v)
+    result += '\n%s}' % (' ' * indent)
+    return result
+
+  def _repr(self, indent=0):
+    f = pformat
+    indent += 2
+    indent1 = indent
+    result = '\n%s%s( config=' % (' ' * indent1, str(self))
+    indent += 2
+    result += self._reprDict(self._config, indent) #f(self._config, indent=indent, width=10)
+    result += ', \n%schildren=[' % (' ' * indent)
+    for c in self._children:
+      result += c._repr(indent)
+      result += ',\n'
+    result += '%s]\n%s)' % (' ' * indent, ' ' * indent1)
+    return result
+
+  def __repr__(self):
+    return self._repr()
+
+  def __str__(self):
+    return 'Node_' + object.__repr__(self).split('at')[1].split('>')[0].strip()
+
 
 class ChildNode(BaseNode):
   def __init__(self, parent):
@@ -66,6 +96,7 @@ class ChildNode(BaseNode):
       return self._config[key]
     else:
       return self.__parent.getConfig(key)
+
 
 class TreeContextStore(object):
   def __init__(self, config_tree_node=None):
@@ -111,25 +142,34 @@ class TreeContextStore(object):
       owner_str = "%s" % owner
     return owner_str
 
+  def __repr__(self):
+    return '%s(%r\n)' % (self.__class__.__name__, self._root_node)
+
+  def __str__(self):
+    return str(self._root_node)
+
 def smokeTestModule():
   from common.log.debugPrint import debugPrint
-  p = debugPrint
+  def p(gc_name, gc, name, owner):
+    debugPrint('%s, %s %s=%s' % (gc_name, gc, name, gc.getConfig(name, owner)))
   def assertAndPrint(gc, name, owner, value):
-    p(gc.getConfig(name, owner))
+    debugPrint(gc.getConfig(name, owner))
     assert(gc.getConfig(name, owner) == value)
   gc = TreeContextStore()
-  gc.setConfig('hello', 10, owner='yo')
-  gc.setConfig('hello2', 12, owner='yo')
-  p(gc.getConfig('hello', owner='yo'))
-  assert(gc.getConfig('hello', owner='yo') == 10)
+  owner = 'yo'
+  gc.setConfig('hello', 10, owner=owner)
+  gc.setConfig('hello2', 12, owner=owner)
+  p('gc', gc, 'hello', owner=owner)
+  assert(gc.getConfig('hello', owner=owner) == 10)
   gc1 = gc.newChildConfig()
-  gc1.setConfig('hello', 11, owner='yo')
-  p(gc1.getConfig('hello', owner='yo'))
-  assert(gc1.getConfig('hello', owner='yo') == 11)
-  p(gc.getConfig('hello', owner='yo'))
-  p(gc1.getConfig('hello2', owner='yo'))
-  gc.setConfig('hello3', 13, owner='yo')
-  p(gc1.getConfig('hello3', owner='yo'))
+  gc1.setConfig('hello', 11, owner=owner)
+  p('gc1', gc1, 'hello', owner=owner)
+  assert(gc1.getConfig('hello', owner=owner) == 11)
+  p('gc', gc, 'hello', owner=owner)
+  p('gc1', gc1, 'hello2', owner=owner)
+  gc.setConfig('hello3', 13, owner=owner)
+  p('gc1', gc1, 'hello3', owner=owner)
+  debugPrint(gc)
 
 if __name__ == "__main__":
   smokeTestModule()
