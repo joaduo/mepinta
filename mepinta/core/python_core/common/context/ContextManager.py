df95bf3f41237cb67f1073a0ffe67950e3646324
jduo <joaduo@gmail.com>
2015-02-05 14:54:43 -0300

    Enhance context resolution and Error reporting

--- mepinta/core/python_core/common/context/ContextManager.py
+++ mepinta/core/python_core/common/context/ContextManager.py
@@ -21,45 +21,62 @@ along with Mepinta. If not, see <http://www.gnu.org/licenses/>.
 from common.abstract.decorators.singleton import singleton_decorator
 from collections import defaultdict
 import threading
+import logging
 
 
 @singleton_decorator
 class ContextManager(object):
     def __init__(self):
+        self._contexts = set()
         self._context_stacks = defaultdict(list)
+        # Caching main thread
+        self._main_thread = None
 
     def getContext(self, thread=None):
         stack = self.getStack(thread)
         if stack:
+            # Return latest context registered for this thread
             return stack[-1]
         else:
-            thread = self._getThread()
             # If we are inside a thread and there is no context defined
             # we get the context of the maint thread
-            if not isinstance(thread, threading._MainThread):
-                for t in self._context_stacks:
-                    if isinstance(t, threading._MainThread):
-                        return self.getContext(t)
+            if not isinstance(self._getThread(), threading._MainThread):
+                return self.getContext(self._getMainThread())
+        logging.warn('No context defined in thread, neither in main thread.')
+        # Make return explicit
+        return None
+
+    def _getMainThread(self):
+        if not self._main_thread:
+            for t in self._context_stacks:
+                if isinstance(t, threading._MainThread):
+                    self._main_thread = t
+        return self._main_thread
 
     def _getThread(self):
         return threading.current_thread()
     
     def getStack(self, thread=None):
-        if not thread:
-            thread = self._getThread()
+        thread = thread or self._getThread()
         return self._context_stacks[thread]
 
     def register(self, context):
-        stack = self._context_stacks[self._getThread()]
-        assert context not in stack or context == stack[-1]
-        if not stack or context != stack[-1]:
-            stack.append(context)
+        stack = self.getStack()
+        # Trying to append the context twice
+        if context in self._contexts:
+            raise ValueError('Context %r already registered' % context)
+        self._contexts.add(context)
+        stack.append(context)
 
     def unregister(self, context):
         stack = self.getStack()
-        if not stack or context != stack[-1]:
-            raise LookupError('Could not solve the right context to be unregistered')
+        if context not in self._contexts:
+            raise LookupError('Context %r is not registered' % context)
+        elif context != stack[-1]:
+            # Context is not registered
+            raise LookupError('Context %r is not the last context in stack' % context)
         else:
+            self._contexts.remove(context)
             stack.pop()
 
 
@@ -70,7 +87,10 @@ def smokeTestModule():
     context = 'foo'
     context2 = 'bar'
     mngr.register(context)
-    mngr.register(context)
+    try:
+        mngr.register(context)
+    except ValueError as e:
+        debugPrint(e)
     mngr.register(context2)
     try:
         mngr.unregister(context)
